f4a7fa4bba3d9c461bb9a9d2ef973931c9475504
zetrocode <zetrocode@hotmail.com>
2023-03-15 09:07:50 -0500

    first commit

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = ""; // Application name
+$ownerid = ""; // Application ownerID
+$version = ""; // Application Version
+$sellerkey = ""; // application's seller key. Found in seller settings (You must have seller plan)
+
+?>
